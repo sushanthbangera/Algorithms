48cf654c7680cf5144f0b7ed0f16252007926c20
sushanthbangera <30630541+sushanthbangera@users.noreply.github.com>
2017-09-10 14:53:35 +0530

    Rotate Array
    Program to rorate an array by 'd' positions towards left

---
+++ src/utils/Utility.java
@@ -0,0 +1,15 @@
+
+package utils;
+
+/**
+ *
+ * @author Sushanth Bangera
+ */
+public class Utility {
+    
+    public static void printArray(int arr[]) {
+        for(int a : arr) {
+            System.out.print(a + " ");
+        }
+    }
+}
