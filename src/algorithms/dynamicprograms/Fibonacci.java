fd78e19594191641147a613094fbad5c7eb673e2
sushanthbangera <30630541+sushanthbangera@users.noreply.github.com>
2020-04-26 14:03:50 +0530

    Update Fibonacci
    Updating Fibonacci DP solutions

--- src/algorithms/dynamicprograms/Fibonacci.java
+++ src/algorithms/dynamicprograms/Fibonacci.java
@@ -16,10 +16,18 @@ public class Fibonacci {
     public static void main(String[] args) {
         int n = 5;
         cache = new int[n + 1];
+        
         fibonacci(n);
+        
+        Utility.printArray(cache); // top down approach doesnt fill all the values
+        
+        System.out.println();
+        
+        cache = fib(n);
         Utility.printArray(cache);
     }
     
+    // Top down uproach - Memoization
     private static int fibonacci(int n) {
         
         if (n < 2) {
@@ -33,4 +41,30 @@ public class Fibonacci {
         return cache[n] = fibonacci(n - 1) + fibonacci(n - 2);
     }
   
+    // Bottom up approach fills all the values
+    private static int[] fib(int n) {
+        
+        int cache[] = new int[n + 1];
+        
+        cache[0] = 0;
+        cache[1] = 1;
+        
+        for (int i = 2; i <= n; i++) {
+            cache[i] = cache[i - 1] + cache[i - 2];
+        }
+        
+        return cache;
+    }
+    
+    private static int simpleFib(int n) {
+        
+        int a = 0, b = 1, c = 0;
+        
+        for (int i = 2; i < n; i++) {
+            c = a + b;
+            a = b;
+            b = c;
+        }
+        return b;
+     }
 }
